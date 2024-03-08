55401c4b56474601424b4ef7e73e4b0f6824a61e
gauravkh31 <khandelwalgaurav3100@gmail.com>
2024-03-08 19:22:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let words = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit', 'sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 'aliqua.', 'Ut', 'enim', 'ad', 'minim', 'veniam', 'quis', 'nostrud', 'exercitation', 'ullamco', 'laboris', 'nisi', 'ut', 'aliquip', 'ex', 'ea', 'commodo', 'consequat.', 'Duis', 'aute', 'irure', 'dolor', 'in', 'reprehenderit', 'in', 'voluptate', 'velit', 'esse', 'cillum', 'dolore', 'eu', 'fugiat', 'nulla', 'pariatur.', 'Excepteur', 'sint', 'occaecat', 'cupidatat', 'non', 'proident', 'sunt', 'in', 'culpa', 'qui', 'officia', 'deserunt', 'mollit', 'anim', 'id', 'est', 'laborum', 'Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipisicing', 'elit', 'sed', 'do', 'eiusmod', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 'aliqua.', 'Ut', 'enim', 'ad', 'minim', 'veniam', 'quis', 'nostrud', 'exercitation', 'ullamco', 'laboris', 'nisi', 'ut', 'aliquip', 'ex', 'ea', 'commodo', 'consequat.', 'Duis', 'aute', 'irure', 'dolor', 'in', 'reprehenderit', 'in', 'voluptate', 'velit', 'esse', 'cillum', 'dolore', 'eu', 'fugiat', 'nulla', 'pariatur.', 'Excepteur', 'sint', 'occaecat', 'cupidatat', 'non', 'proident', 'sunt', 'in', 'culpa', 'qui', 'officia', 'deserunt', 'mollit', 'anim', 'id', 'est', 'laborum'];
+
+let paragraph= "";
+let number = prompt("Enter the number of words, You want to practice:")
+let count=0;
+
+
+if(number<=1000000){
+for(let i=0; i<=number; i++){
+    paragraph= paragraph+ words[Math.floor(Math.random() * words.length)]+ " ";
+}
+
+let yourMsg= `OKAY!  I've generated ${number} words for you...`
+
+
+let elementHeading = document.createElement("h1")
+elementHeading.innerText = yourMsg
+
+let elementParagraph= document.createElement("h2")
+elementParagraph.innerText= paragraph
+
+let heading = document.querySelector('.heading')
+let paragraph1 = document.querySelector('.paragraph')
+
+paragraph1.prepend(elementParagraph)
+heading.prepend(elementHeading)
+}
+
+else{
+
+    let condition = "Please enter a number less than 1000000. It hangs my system LOL."
+    let elementHeading = document.createElement("h1")
+    elementHeading.innerText = condition;
+    let heading = document.querySelector('.heading')
+    heading.prepend(elementHeading)
+
+
+}
